2f55518dbeb40fdff322902cf3167226d80b322e
Eric Simonson <esimonson@email.arizona.edu>
2023-04-24 13:31:56 -0700

    Added import

--- bot_detection/src/main/java/final_project/QueryEngine.java
+++ bot_detection/src/main/java/final_project/QueryEngine.java
@@ -31,6 +31,7 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import java.util.ArrayList;
